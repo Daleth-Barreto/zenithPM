649e3d9fc4f99c7d8d623538daf0573ed2595183
Alan Daleth Hernández Barreto <alandaleth.hb@gmail.com>
2025-08-03 03:51:58 +0000

    otro diferente, se parece mucho al de vercel

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,7 +8,11 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       fill="currentColor"
       {...props}
     >
-      <path d="M3.5 18.5 12 5l8.5 13.5H3.5Zm3.25-1.5h10.5L12 8.25 6.75 17Z" />
+      <path
+        fillRule="evenodd"
+        d="M12 2a10 10 0 100 20 10 10 0 000-20zM8.5 7H14a1.5 1.5 0 011.5 1.5v1.25a.75.75 0 01-1.5 0V8.5a.5.5 0 00-.5-.5H9.75l4.5 6.25H10a1.5 1.5 0 01-1.5-1.5v-1.25a.75.75 0 011.5 0v1.25a.5.5 0 00.5.5h2.25L7 8.25V17H8.5V7z"
+        clipRule="evenodd"
+      />
     </svg>
   );
 }

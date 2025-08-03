b5cfa8770b89fc80cf1af8c52c780916f17af02e
Alan Daleth Hernández Barreto <alandaleth.hb@gmail.com>
2025-08-03 03:16:41 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC_MIXBzCO62thFFtTnDlUQJCdWRHOXVcc",
+  authDomain: "zenith-80fae.firebaseapp.com",
+  projectId: "zenith-80fae",
+  storageBucket: "zenith-80fae.firebasestorage.app",
+  messagingSenderId: "646291807012",
+  appId: "1:646291807012:web:8a0614f831ab84d95b71b1",
+  measurementId: "G-9C19CLJQ8D"
 };
 
 // Initialize Firebase

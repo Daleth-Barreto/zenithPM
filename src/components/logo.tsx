60ea2a31523521da55e66bd61a817d0a8a817458
Alan Daleth Hernández Barreto <alandaleth.hb@gmail.com>
2025-08-03 03:53:27 +0000

    minimalista y abstracto

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,11 +8,7 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       fill="currentColor"
       {...props}
     >
-      <path
-        fillRule="evenodd"
-        d="M12 2a10 10 0 100 20 10 10 0 000-20zM8.5 7H14a1.5 1.5 0 011.5 1.5v1.25a.75.75 0 01-1.5 0V8.5a.5.5 0 00-.5-.5H9.75l4.5 6.25H10a1.5 1.5 0 01-1.5-1.5v-1.25a.75.75 0 011.5 0v1.25a.5.5 0 00.5.5h2.25L7 8.25V17H8.5V7z"
-        clipRule="evenodd"
-      />
+      <path d="M5 3H14L9 11H19L10 21H6L15 13H5L14 3Z" />
     </svg>
   );
 }

95dda9d7e4a3f9fcdd35c69bfc7b585bd773d663
Alan Daleth Hernández Barreto <alandaleth.hb@gmail.com>
2025-08-03 19:52:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/tasks/task-details-sheet.tsx
+++ src/components/tasks/task-details-sheet.tsx
@@ -55,6 +55,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { cn } from '@/lib/utils';
 import { useAuth } from '@/contexts/auth-context';

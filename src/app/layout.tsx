a0182c4591660f60609f24d18947b202ec596712
Alan Daleth Hernández Barreto <alandaleth.hb@gmail.com>
2025-08-04 03:25:15 +0000

    feat: add a splash screen to pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,8 +49,18 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#1e293b" />
+        <meta name="application-name" content="ZenithPM" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="ZenithPM" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#1c1917" />
+        
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </head>
       <body>
         <ThemeProvider
